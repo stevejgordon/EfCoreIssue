dcb0ada49ade40da840fa58f33403b50591f27b7
Steve Gordon <steve.gordon@madgex.com>
2017-08-18 16:00:21 +0100

    Initial

---
+++ Version1/TestContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Version1
+{
+    public class TestContext : DbContext
+    {
+        public TestContext()
+        {
+        }
+
+        public TestContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            Map(modelBuilder.Entity<Book>());
+            Map(modelBuilder.Entity<Author>());
+        }
+
+        private static void Map(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.Author).WithMany(x => x.Books).HasForeignKey(x => x.AuthorId);
+        }
+
+        private static void Map(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasMany(x => x.Books);
+        }
+    }
+}
---
+++ Version1/UnitTest1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Version1
+{
+    public class UnitTest1
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UnitTest1()
+        {
+            var services = new ServiceCollection();
+
+            // set up empty in-memory test db
+            services
+                .AddEntityFrameworkInMemoryDatabase()
+                .AddDbContext<TestContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemory").UseInternalServiceProvider(services.BuildServiceProvider());
+                    options.EnableSensitiveDataLogging();
+                });
+
+            // set up service provider for tests
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            using (var ctx = _serviceProvider.GetService<TestContext>())
+            {
+                // Add a book without an author
+                ctx.Books.Add(new Book{ Id = 1 });
+                ctx.SaveChanges();
+                
+                var book = ctx.Books.Include(x => x.Author).FirstOrDefault(x => x.Id == 1);
+
+                Assert.NotNull(book);
+            }
+        }
+    }
+}
